807306eb96804f76baa74953b9c6836f7dacf625
TUEXU <53074494+TUEXU@users.noreply.github.com>
2023-05-22 09:31:40 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -71,6 +71,14 @@ const USER_CONFIG = {
         //{ keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
+    {
+      // 想要发送的人的名字
+      name: '自己',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'PDU20042T9LDyJ9gRsUgdp2choANrSEId5GBJc4gb',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: '0002',
+    },
   ],
 
 
