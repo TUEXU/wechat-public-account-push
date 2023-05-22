42e795528af935d2fe24ece15755ccf4d1541e79
TUEXU <53074494+TUEXU@users.noreply.github.com>
2023-05-22 10:13:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -45,7 +45,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '老婆',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU20042T9LDyJ9gRsUgdp2choANrSEId5GBJc4gb',
+      id: 'PDU22835THcF5cNWvK5ohDxjDxNCuiy0kcJrF1Ark',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
