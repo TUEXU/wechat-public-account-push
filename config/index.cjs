5e53a462d50706c50d1f23d848523f738ca3f17d
TUEXU <53074494+TUEXU@users.noreply.github.com>
2023-05-22 09:11:43 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,6 +6,8 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
+  
+  USE_PASSAGE: 'push-deer',
 
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx24ab7b8689b3a653',
@@ -43,9 +45,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '老婆',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o-kz25h0dz1GCIy97r9A-3Cob_yY',
+      id: 'PDU20042T9LDyJ9gRsUgdp2choANrSEId5GBJc4gb',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'QA1gpBcbffd2YC7qFmc5RhEocxPKbtOZuf-yGSDUjYc',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-09',
       festivals: [
